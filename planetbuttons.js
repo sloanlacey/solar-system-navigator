317e04f5b8c6eb6bd566923208458e2f33b4a072
sloanlacey <sloanlacey89@gmail.com>
2020-12-24 19:11:55 -0800

    Add Mars and Jupiter pages including HTML and CSS, update JS file for planet buttons to create functionality on new pages.

--- planetbuttons.js
+++ planetbuttons.js
@@ -6,3 +6,11 @@ $('#earth-button').click(function() {
 $('#moon-button').click(function() {
     window.location = 'file:///Users/io/code/Project-1/moon.html';
 })
+
+$('#mars-button').click(function() {
+    window.location = 'file:///Users/io/code/Project-1/mars.html';
+})
+
+$('#jupiter-button').click(function() {
+    window.location = 'file:///Users/io/code/Project-1/jupiter.html';
+})
