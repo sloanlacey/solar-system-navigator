17240e65c8972f76404375024147c6b1ed4f58af
Michael Alexander <michael.m.alexander@outlook.com>
2020-12-31 12:49:07 -0800

    added wiki functionality to remaining planet pages

---
+++ uranus.js
@@ -0,0 +1,35 @@
+function shortenWikiHtmlData(htmlData) {
+    const headerTagAt = /<h[0-9]>/g;
+    const hTagIndex = htmlData.search(headerTagAt);
+    return htmlData.substring(0, hTagIndex);
+}
+
+function wikiAPIHandler(data, subject){
+    const key = Object.keys(data.query.pages);
+    const htmlData = data.query.pages[key[0]].extract;
+    const wikiData = shortenWikiHtmlData(htmlData);
+    const title = subject.replace(subject.charAt(0), subject.charAt(0).toUpperCase());
+    renderToModal(wikiData, title);
+}
+
+function startWikiAjax(subject) {
+    const url = `https://en.wikipedia.org/w/api.php?action=query&titles=${subject}&prop=extracts&format=json&origin=*`;
+    
+    function makeRequest() {
+        const httpRequest = new XMLHttpRequest();
+
+        httpRequest.onreadystatechange = function(){
+            if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200){
+                wikiAPIHandler(JSON.parse(httpRequest.responseText), subject);
+            } 
+        };
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    }
+    makeRequest();
+}
+
+document.getElementById("wiki-button").addEventListener("click", () => {    
+    startWikiAjax("uranus");
+    renderToModal(getPreloader());
+})
