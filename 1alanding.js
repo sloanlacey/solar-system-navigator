a24e41d6016c3d1ca065f921e8f9b29c801f203e
Aaron Alexander <aaronalexander@Aarons-Air.hsd1.wa.comcast.net>
2020-12-30 17:11:05 -0800

    text

---
+++ 1alanding.js
@@ -0,0 +1,7 @@
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+  // setInterval(function(){
+  //   $('.carousel.carousel-slider').carousel('next')
+  // },4000);
