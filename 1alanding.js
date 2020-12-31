5cf090476bd7f96d71abea0d12322e8958384494
e4k9 <eh26steve@gmail.com>
2020-12-30 17:51:32 -0800

    land changes

--- 1alanding.js
+++ 1alanding.js
@@ -2,6 +2,10 @@ $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true
   });
-  // setInterval(function(){
-  //   $('.carousel.carousel-slider').carousel('next')
-  // },4000);
+  setInterval(function(){
+    $('.carousel.carousel-slider').carousel('next')
+  },4000);
+
+  $(document).ready(function() {
+    $('.sidenav').sidenav();
+    });
