039224d6b37cf60e9f70f58476802e350de09d57
sloanlacey <sloanlacey89@gmail.com>
2020-12-30 16:54:48 -0800

    Resolve merge conflict.

--- planetbuttons.js
+++ planetbuttons.js
@@ -6,11 +6,40 @@ $('#earth-button').click(function() {
 $('#moon-button').click(function() {
     window.location = './moon.html';
 })
-
+// Mars button
 $('#mars-button').click(function() {
     window.location = './mars.html';
 })
-
+// Jupiter button
 $('#jupiter-button').click(function() {
     window.location = './jupiter.html';
 })
+// Sun button
+$('#sun-button').click(function() {
+    window.location = './sun.html';
+})
+// Mercury button
+$('#mercury-button').click(function() {
+    window.location = './mercury.html';
+})
+// Venus button
+$('#venus-button').click(function() {
+    window.location = './venus.html';
+})
+// Saturn button
+$('#saturn-button').click(function() {
+    window.location = './saturn.html';
+})
+// Uranus button
+$('#uranus-button').click(function() {
+    window.location = './uranus.html';
+})
+// Neptune button
+$('#neptune-button').click(function() {
+    window.location = './neptune.html';
+})
+// Pluto button
+$('#pluto-button').click(function() {
+    window.location = './pluto.html';
+})
+
